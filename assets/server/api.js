245eddd24e62aee4eca7bf4d422ff3be11e2c214
Promlert Lovichit <promlert@gmail.com>
2020-05-12 15:31:21 +0700

    ทดสอบ Node.js api ดึงข้อมูลจาก mysql

--- assets/server/api.js
+++ assets/server/api.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'Exf@2020ch5U$m#2kh&Mc[XY',
+    database : 'itsexat2020'
+});
 const port = 3000;
 
 app.get('/',
-    (req, res) => res.send('Hello Node.JS !')
+    (req, res) => {
+        connection.connect();
+
+        connection.query('SELECT name FROM gate_in', function (error, results, fields) {
+            if (error) throw error;
+
+            const tableRows = results.reduce((total, row) => {
+                return total += `<tr><td>${row.name}</td></tr>`;
+            }, '');
+            res.send(`<table>${tableRows}</table>`);
+        });
+
+        connection.end();
+    }
 );
 
 app.listen(port, () => console.log(`API listening at http://localhost:${port}`));
