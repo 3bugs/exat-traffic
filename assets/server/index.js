21cf133db5b5ef74606ad8ec3d74548751264cd4
Promlert Lovichit <promlert@gmail.com>
2020-05-31 00:33:36 +0700

    เพิ่มพารามิเตอร์ type สำหรับ visualization api

--- assets/server/index.js
+++ assets/server/index.js
@@ -17,25 +17,39 @@ app.use(express.static('./'));
 });*/
 
 app.get('/location', function (req, res) {
-    const {lat, lng} = req.query;
-    //io.emit('location', `Latitude: ${lat}, Longitude: ${lng}`);
-    io.emit('location', {lat: parseFloat(lat), lng: parseFloat(lng)});
-    console.log(`Latitude: ${lat}, Longitude: ${lng}`);
+    const {lat, lng, type} = req.query;
+    if (lat == null || lng == null || type == null) {
+        res.json({error: {code: 1, message: 'ส่งพารามิเตอร์มาไม่ครบ ต้องมี lat, lng, type'}});
+        return;
+    }
 
-    res.send('ok');
+    const latValue = parseFloat(lat);
+    const lngValue = parseFloat(lng);
+    if (isNaN(latValue) || isNaN(lngValue)) {
+        res.json({error: {code: 2, message: 'พารามิเตอร์ lat หรือ lng ไม่ใช่ตัวเลข'}});
+        return;
+    }
+    if (latValue > 90 || latValue < -90 || lngValue > 180 || lngValue < -180) {
+        res.json({error: {code: 3, message: 'ค่าของพารามิเตอร์ lat หรือ lng เกินขอบเขต (lat ต้องอยู่ในช่วง -90 ถึง 90, lng ต้องอยู่ในช่วง -180 ถึง 180)'}});
+        return;
+    }
+    const validTypes = ['inuse', 'bg']
+    if (!validTypes.includes(type.toString().toLowerCase())) {
+        res.json({error: {code: 4, message: 'พารามิเตอร์ type ไม่ถูกต้อง, ต้องมีค่า inuse หรือ bg เท่านั้น'}});
+        return;
+    }
+
+    console.log(`Latitude: ${lat}, Longitude: ${lng}`);
+    io.emit('location', {lat: latValue, lng: lngValue, type});
+    res.json({error: {code: 0, message: 'success: ยิงข้อมูล lat, lng ไปยังหน้าเว็บ Visualization สำเร็จ'}});
 });
 
 io.on('connection', function (socket) {
-    console.log('a client connected');
-
-    /*socket.on('chat message', function (msg) {
-        console.log('message: ' + msg);
-        io.emit('chat message', msg);
-    });
+    console.log('client connected');
 
     socket.on('disconnect', function () {
-        console.log('user disconnected');
-    });*/
+        console.log('client disconnected');
+    });
 });
 
 http.listen(3000, function () {
