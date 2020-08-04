8bcc1cc72ec480e6d8f2f2dceaad8921ed6f8329
Promlert Lovichit <promlert@gmail.com>
2020-08-04 11:16:33 +0700

    app: finish toll plaza bottom sheet web: ปรับสีเส้น schematic maps

--- lib/models/core_configs_model.dart
+++ lib/models/core_configs_model.dart
@@ -4,11 +4,13 @@ class CoreConfigModel {
   final int id;
   final String code;
   final String value;
+  final String name;
 
   CoreConfigModel({
     @required this.id,
     @required this.code,
     @required this.value,
+    @required this.name,
   });
 
   factory CoreConfigModel.fromJson(Map<String, dynamic> json) {
@@ -16,11 +18,12 @@ class CoreConfigModel {
       id: json['id'],
       code: json['code'],
       value: json['value'],
+      name: json['name'],
     );
   }
 
   @override
   String toString() {
-    return 'ID: $id, Code: $code, Value: $value';
+    return 'ID: $id, Code: $code, Value: $value, Name: $name';
   }
 }
--- lib/screens/home/bloc/marker_bloc.dart
+++ lib/screens/home/bloc/marker_bloc.dart
@@ -19,21 +19,61 @@ class MarkerBloc extends Bloc<MarkerEvent, MarkerState> {
       // แปลง MarkerModel เป็น TollPlazaModel
       yield ShowTollPlazaBottomSheet(
         tollPlaza: TollPlazaModel(
-            name: marker.name,
-            imageUrl: marker.imagePath,
-            cost4Wheels: _getFeeValue(marker.coreConfigList, "4"),
-            cost6To10Wheels: _getFeeValue(marker.coreConfigList, "6-10"),
-            costOver10Wheels: _getFeeValue(marker.coreConfigList, "over10"),
+          name: marker.name,
+          imageUrl: marker.imagePath,
+          cost4Wheels: _getFeeValue(marker.coreConfigList, "4"),
+          cost6To10Wheels: _getFeeValue(marker.coreConfigList, "6-10"),
+          costOver10Wheels: _getFeeValue(marker.coreConfigList, "over10"),
+          laneList: _getLaneList(marker.coreConfigList),
         ),
       );
     }
   }
 
   int _getFeeValue(List<CoreConfigModel> coreConfigList, String type) {
-    List<CoreConfigModel> list = coreConfigList.where((coreConfig) => coreConfig.code == "4")
-        .toList();
+    List<CoreConfigModel> list =
+    coreConfigList.where((coreConfig) => coreConfig.code == "4").toList();
     return (list.isNotEmpty && list[0].value != null && list[0].value
         .trim()
-        .isNotEmpty) ? int.parse(list[0].value) : -1;
+        .isNotEmpty)
+        ? int.parse(list[0].value)
+        : -1;
+  }
+
+  List<TollPlazaLaneModel> _getLaneList(List<CoreConfigModel> coreConfigList) {
+    List<TollPlazaLaneModel> laneList = List();
+
+    print("******************** LANE ********************");
+    coreConfigList
+        .where((coreConfig) => coreConfig.code == "markers_channel")
+        .forEach((coreConfig) {
+      /*final intRegex = RegExp(r'\s+(\d+)\s+', multiLine: true);
+      List regexLaneNumberList =
+          intRegex.allMatches(coreConfig.name).map((m) => m.group(0)).toList();*/
+
+      TollPlazaLaneType laneType;
+      switch (coreConfig.value.toUpperCase()) {
+        case "M":
+          laneType = TollPlazaLaneType.cash;
+          break;
+        case "E":
+          laneType = TollPlazaLaneType.easyPass;
+          break;
+      }
+
+      int laneNumber = int.tryParse(coreConfig.name.replaceAll("ช่อง", "")) ?? 0;
+      assert(laneNumber > 0);
+
+      if (laneType != null && laneNumber > 0) {
+        TollPlazaLaneModel lane = TollPlazaLaneModel(
+          number: laneNumber,
+          type: laneType,
+        );
+        laneList.add(lane);
+        print(lane);
+      }
+    });
+
+    return laneList;
   }
 }
