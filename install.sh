70fdfa08d8c300c45f61e169a07deddc8f5b8565
Promlert Lovichit <promlert@gmail.com>
2020-09-17 02:21:48 +0700

    server: กำหนดสีเส้น default ให้กับ schematic maps

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd ~/Projects/2fellows/EXAT-Traffic/app/exat_traffic/build/app/outputs/apk/release || exit
-adb -e uninstall isymphonyz.exat
-adb -e install app-armeabi-v7a-release.apk
+adb -d uninstall isymphonyz.exat
+adb -d install app-armeabi-v7a-release.apk
